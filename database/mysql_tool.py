8a668a60d0a1420e9db16bbc69c8011f29a2aa19
刘荣江 <774726704@qq.com>
2018-08-13 09:56:33 +0800

    add except catch

--- database/mysql_tool.py
+++ database/mysql_tool.py
@@ -13,27 +13,36 @@ class MysqlHandler():
 
     def query(self, query_sql):
         conn=self.pool.get_connection()
-        with conn as cursor:
-            cursor.execute(query_sql)
-            description = cursor.description
-            if description:
-                keys=[]
-                for tup in description:
-                    keys.append(tup[0])
-            fetch = cursor.fetchall()
-            results=[]
-            for res_tup in fetch:
-                dic={}
-                for index in range(0, len(res_tup)):
-                    dic[keys[index]]=res_tup[index]
-                results.append(dic)
-            return results
+        try:
+            with conn as cursor:
+                cursor.execute(query_sql)
+                description = cursor.description
+                if description:
+                    keys=[]
+                    for tup in description:
+                        keys.append(tup[0])
+                fetch = cursor.fetchall()
+                results=[]
+                for res_tup in fetch:
+                    dic={}
+                    for index in range(0, len(res_tup)):
+                        dic[keys[index]]=res_tup[index]
+                    results.append(dic)
+                return results
+        except Exception as e:
+            print('___err_sql: ', query_sql)
+            print( e )
+            return []
 
     def update(self, update_sql):
         conn=self.pool.get_connection()
-        with conn as cursor:
-            cursor.execute(update_sql)
-            conn.commit()
+        try:
+            with conn as cursor:
+                cursor.execute(update_sql)
+                conn.commit()
+        except Exception as e:
+            print('___err_sql: ', update_sql)
+            print( e )
         
     def insert(self, inset_sql):
         self.update(inset_sql)
