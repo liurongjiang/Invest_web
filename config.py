e534fd14494d39859a76b8fc76400fbbe2d749f0
Sing Swee Yang <sweeyangsing@gmail.com>
2018-08-14 14:09:38 +0800

    asd

---
+++ config.py
@@ -0,0 +1,8 @@
+SECRET_KEY = b'\x1c\x94\x8cZ\xf6\xb0e@\xee\xd2\xb3\x8c\x87\xc3\x0e\xa8\xbd\x04 =\x13\xe3\x13\x0b'
+DEBUG = True
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://rongjiang:password4321@192.168.1.180:3306/integrated'
+
+AUTH_SERVER = '192.168.1.52'
+AUTH_SECRET = b'matrixchina'
+NAS_ID = "data_test"
