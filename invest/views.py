0085f86fc49bc2848e880355560aea74cd7a74f0
sweeyang <sweeyangsign@gmail.com>
2018-08-15 17:42:09 +0800

    update tableName

--- invest/views.py
+++ invest/views.py
@@ -3,11 +3,13 @@ import re, json, yaml
 from invest import invest
 from util.sql_tools import *
 from flask import request, render_template
-from database.my_mysql import MysqlHandle
-
-mysql_settings=yaml.load(open('./yamls/mysql.yaml'))
-mysql=MysqlHandle(mysql_settings)
+from database.mysql_tool import MysqlHandler
+#from database.my_mysql import MysqlHandle
+#mysql_settings=yaml.load(open('./yamls/mysql.yaml'))
+#mysql=MysqlHandle(mysql_settings)
 from flask_login import current_user,login_required
+
+mysql=MysqlHandler()
 
 @invest.route('/list', methods=('GET', 'POST'))  #指定路由为/，因为run.py中指定了前缀，浏览器访问时，路径为http://IP/asset/
 @login_required
@@ -18,7 +20,7 @@ def invest_list():
 @invest.route('/invest_json', methods=('GET', 'POST'))
 def invest_json():
     args=request.args
-    table_name='invest_event_info'
+    table_name='integrated_company'
     
     query_sql, count_sql = query_list(table_name, args)
     docs = mysql.query(query_sql)
