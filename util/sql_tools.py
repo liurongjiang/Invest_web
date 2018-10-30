911dc4c270dc33dd89f2de458b0aed29c73970a5
liurongjiang <774726704@qq.com>
2018-10-30 15:38:51 +0800

    uper

--- util/sql_tools.py
+++ util/sql_tools.py
@@ -84,7 +84,7 @@ def query_list(table_name, args):
     if WHERES:
         WHERE += ' WHERE ' + ' AND '.join(WHERES) + ' AND turn_level != ""'
     ORDER_BY=' ORDER BY %s %s' % (ORDER_KEY, ORDER_DIR)
-    if SOURCE.strip() != '工商':
+    if SOURCE and SOURCE.strip() != '工商':
         WHERE=WHERE or ' WHERE turn_level != "" '
 
     query_sql='SELECT * ' + FROM + WHERE + ORDER_BY + LIMIT + ';'
